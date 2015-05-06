e3ef7366e9be405062e164edf0754315552f31e6
unknown <tblais@WIN-TBLAIS-S1.adprod.bmc.com>
2015-05-06 13:52:00 -0400

    dded basic angular app structure

---
+++ js/app.js
@@ -0,0 +1,20 @@
+"use strict";
+
+(function(){
+	function appConfig( $compileProvider, $stateProvider, $urlRouterProvider ){
+
+		// Un comment if you need to include link to mail, skype, phone or other
+		//$compileProvider.aHrefSanitizationWhitelist(/^\s*(https?|ftp|mailto|skype|tel):/);
+
+		// For any unmatched url, redirect to /home
+  		$urlRouterProvider.otherwise("/home");
+
+  		$stateProvider
+  			.state('home', { url: "/home", templateUrl: "views/home.html" });
+
+	}
+
+	angular.module('app', ['ngResource', 'ui.router', 'ui.bootstrap'])
+		.config(['$compileProvider', '$stateProvider', '$urlRouterProvider', appConfig]);
+
+})();
---
+++ js/controllers/MainCtrl.js
@@ -0,0 +1,15 @@
+
+
+(function(){
+	"use strict";
+
+	function MainCtrl(){
+		var MainCtrl = this;
+	}
+
+	angular	.module('app')
+			.controller('MainCtrl', [MainCtrl]);
+
+
+
+})();
